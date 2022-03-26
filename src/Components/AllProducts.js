5ac44f0476235410d1b1bfcdf9ddab89352e542c
kmminhaz <kmminhaj.khan@gmail.com>
2022-03-26 23:12:45 +0600

    Add to cart done

--- src/Components/AllProducts.js
+++ src/Components/AllProducts.js
@@ -21,7 +21,7 @@ const AllProducts = (props) => {
 
     return (
       <div className='flex'>
-        <div className='grid-type spacing'>
+        <div className='grid-type spacing' id='products'>
           {products.map((product) => (
             <ShopProduct
               id={product.id}
@@ -30,19 +30,24 @@ const AllProducts = (props) => {
             ></ShopProduct>
           ))}
         </div>
-        <div className='spacing'>
+        <div className='spacing' id='cart'>
           <h1>Selected Shoes</h1>
           <h3>{cart[0]}</h3>
           <h3>{cart[1]}</h3>
           <h3>{cart[2]}</h3>
           <h3>{cart[3]}</h3>
-          {/* {console.log(cart[0])} */}
-          <button className='button' style={{ color: "green" }}>
+          
+          <button onClick={generateRandom} className='button' style={{ color: "green" }}>
             Choos 1 For Me
           </button>
         </div>
       </div>
     );
 };
+
+function generateRandom(){
+    const randomValue = Math.floor(Math.random() * (3 - 0) ) + 0;
+    console.log(randomValue);
+}
 
 export default AllProducts;
